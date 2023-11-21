dfb1913e44fc394cdf239b4a7b2b0f48a0fe80c3
Leonardo Davalos <32350741+leothecrz@users.noreply.github.com>
2023-11-20 16:51:59 -0800

    txt parse init

--- SentimentAnalysis/views.py
+++ SentimentAnalysis/views.py
@@ -94,6 +94,37 @@ def analyzeText():
     else:
         return render_template('main.html', USERNAME=session["username"], sentiment="Failed to analyze sentiment", input_text=text)
 
+def finishFileProcess(identifiers, messages):
+    JSONData = json.dumps(messages)
+    B64Data = base64.b64encode( JSONData.encode() ).decode()
+    APIPayload =  {
+        'queryType' : 'multiple',
+        'query' : B64Data
+    }
+
+    response = requests.get(SENTIMENT_API_URL, params=APIPayload)
+    
+    if response.status_code == 200:
+        resJSONData = response.json()['body']
+        resJSONData = resJSONData.split('\n', 1)[1]
+        splitJSONData = resJSONData.split('\n')
+        splitJSONData.pop()
+        
+        sentTable = []
+        i = 0
+        for splitString in splitJSONData:
+            activeSplit = splitString.rsplit(',',1)
+            
+            nextEntry = {'index':i, 'indentifier':identifiers[i], 'query':activeSplit[0], 'result': activeSplit[1]}
+            i = i+1
+            sentTable.append(nextEntry)
+
+        return render_template('main.html', USERNAME=session["username"], sentimentTable=sentTable )
+
+    else:
+        print("Failed. CODE: ", response.status_code) 
+        return
+
 @views.route('/analyzeFile', methods=['POST'])
 def analyzeFile():
     try: 
@@ -110,72 +141,49 @@ def analyzeFile():
         if( not ( ('.' in activeFileName) and (fileExtention in VALID_EXTENSIONS) ) ):
             return render_template('main.html', USERNAME=session["username"], ERRORMESSAGE="File Extension is not allowed")
         
+        ##VARS
+        messages = []
+        identifiers = []
         fileContent = activeFile.read().decode('utf-8')
-        if(fileExtention == 'txt'):
+        
+        if(fileExtention == 'txt'): #TXT
             print(fileContent)
             #txt parse
-
-
+            try:
+                lines = fileContent.split('\n')
+                for line in lines:
+                    if not line.strip():
+                        continue  # Skip empty lines
+            except Exception as e:
+                print("error")
+            
             ##Unfinished
             return render_template('main.html', USERNAME=session["username"], ERRORMESSAGE="File Extension is txt")
-        elif (fileExtention == 'csv'):
+        elif (fileExtention == 'csv'): #CSV
             csvRecords = []
             for row in csv.reader(io.StringIO(fileContent)):
                 csvRecords.append(row)
+            
+            if len(csvRecords[0]) > 2:
+                return render_template('main.html', USERNAME=session["username"], ERRORMESSAGE="CSV formatting issue. Only use two columns")
+
             #csv parse
-            messages = []
             for rec in csvRecords:
+                identifier = rec[0]
                 message = rec[1]
                 messages.append(message)
-
-            #cvs records[i][0] holds identifer
-            #cvs messages[i] holds message
-
-            JSONData = json.dumps(messages)
-            B64Data = base64.b64encode( JSONData.encode() ).decode()
-            APIPayload =  {
-                'queryType' : 'multiple',
-                'query' : B64Data
-            }
-
-            response = requests.get(SENTIMENT_API_URL, params=APIPayload)
-            
-            if response.status_code == 200:
-                resJSONData = response.json()['body']
-                resJSONData = resJSONData.split('\n', 1)[1]
-                splitJSONData = resJSONData.split('\n')
-                splitJSONData.pop()
-                print(splitJSONData)
-                
-                sentTable = []
-                i = 0
-                for splitString in splitJSONData:
-                    activeSplit = splitString.rsplit(',',1)
-                    print(activeSplit)
-                    print(i)
-                    print(csvRecords[i])
-                    nextEntry = {'index':i, 'indentifier':csvRecords[i][0], 'query':activeSplit[0], 'result': activeSplit[1]}
-                    i = i+1
-                    sentTable.append(nextEntry)
-
-            
-                return render_template('main.html', USERNAME=session["username"], sentimentTable=sentTable )
-            else:
-                print("Failed. CODE: ", response.status_code)
-
-            ##Unfinished
-            return render_template('main.html', USERNAME=session["username"], ERRORMESSAGE="File Extension is csv. Processing Failed")
-
-        ##FAILED STATE SHOULD BE UNREACHABLE
-        return render_template('main.html', USERNAME=session["username"], ERRORMESSAGE="File UPLOADED file type not valid. Unknow Error Occurred")
+                identifiers.append(identifier)
+        else: ##FAILED STATE SHOULD BE UNREACHABLE
+            return render_template('main.html', USERNAME=session["username"], ERRORMESSAGE="File UPLOADED file type not valid. Unknow Error Occurred")
+        
+        return finishFileProcess(identifiers, messages)
+    
     except RequestEntityTooLarge:
         #abort(413, 'File size exceeds the allowed limit')
         #MAX FILE SIZE CHECK
         return render_template('main.html', USERNAME=session["username"], ERRORMESSAGE="File size exceeds the allowed limit")
 
-            
 
-        
 
         
 
